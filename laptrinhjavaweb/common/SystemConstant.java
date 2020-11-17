49af8d6d67d62184ebf4111c4c49703df55bc367
Nguyen Cong Hau <63437089+NHau2803@users.noreply.github.com>
2020-11-17 19:05:04 +0700

    Update SystemConstant.java

--- laptrinhjavaweb/common/SystemConstant.java
+++ laptrinhjavaweb/common/SystemConstant.java
@@ -4,8 +4,8 @@ public class SystemConstant {
 	
 	public static final String url = "jdbc:mysql://localhost:3306/website";
 	public static final String nameDB = "website";
-	public static final String user = "hau";
-	public static final String pass = "0946431287mysql";
+	public static final String user = "";
+	public static final String pass = "";
 	public static final String urlDriver = "com.mysql.cj.jdbc.Driver";
     
 
